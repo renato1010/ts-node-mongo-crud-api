4d58dec337a3425deacb984f5de73ba2673b919a
renato.perez <renatoperezc@gmail.com>
2021-07-17 17:10:36 -0600

    Add cors middleware to express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { postRouter } from "./Routers";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
